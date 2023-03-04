567ce4c2e25f353e0d4d05d9b4d5a693e5dd497e
YongHun Lee <yosiyong@gmail.com>
2023-03-04 22:34:49 +0900

    Add my listings section to the profile page and create the listingitem component.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
 import { db, auth } from "../firebase";
 import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { useTranslation } from "react-i18next";
 import { FcHome } from "react-icons/fc";
-import {
-  doc,
-  updateDoc,
-} from "firebase/firestore";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const { t } = useTranslation();
@@ -16,6 +24,8 @@ export default function Profile() {
   //const auth = getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -28,6 +38,7 @@ export default function Profile() {
     navigate("/");
   }
 
+  //編集イベント
   function onChange(e) {
     setFormData((prevState)=>({
       ...prevState,
@@ -35,11 +46,13 @@ export default function Profile() {
     }))
   }
 
+  //登録ボタンクリック
   function onSubmitClick() {
     changeDetail && onSubmit();
     setChangeDetail((prevState) => !prevState);
   }
 
+  //登録ボタン処理（名前とメールアドレス更新）
   async function onSubmit() {
     try {
       if(auth.currentUser.displayName !== name) {
@@ -62,6 +75,35 @@ export default function Profile() {
       toast.error(t("profile error"));
     }
   }
+
+  //auth.currentUser.uidが変更された場合、実施
+  useEffect(()=>{
+    //Listingsデータ取得
+    async function fetchUserListings() {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+
+      //取得データを配列(listings)に格納
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      //取得データ配列をuseStateに格納
+      setListings(listings);
+
+      setLoading(false);
+    }
+    fetchUserListings();
+  },[auth.currentUser.uid]);
 
   return (
     <>
@@ -97,6 +139,18 @@ export default function Profile() {
         </button>
       </div>
     </section>
+    <div className="max-w-6xl px-3 mt-6 mx-auto">
+    {!loading && listings.length > 0 && (
+      <>
+        <h2 className="text-2xl text-center font-semibold">{t('My Listings')}</h2>
+        <ul>
+          {listings.map((listing)=>(
+            <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+          ))}
+        </ul>
+      </>
+    )}
+    </div>
     </>
   )
 }
