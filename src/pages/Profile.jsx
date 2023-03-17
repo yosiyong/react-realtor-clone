88d86eb28d742111094471963b9c914a0a3a85ff
YongHun Lee <yosiyong@gmail.com>
2023-03-17 23:01:09 +0900

    Add delete and edit functionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -96,7 +96,7 @@ export default function Profile() {
           data: doc.data(),
         });
       });
-
+     
       //取得データ配列をuseStateに格納
       setListings(listings);
 
@@ -104,6 +104,23 @@ export default function Profile() {
     }
     fetchUserListings();
   },[auth.currentUser.uid]);
+
+  //目録から指定物件情報を削除する。
+  async function onDelete(listingID) {
+    if (window.confirm(t("Are you sure you want to delete?"))) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success(t("Successfully deleted the listing"));
+    }
+  }
+
+  //編集画面へ遷移
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
 
   return (
     <>
@@ -139,18 +156,27 @@ export default function Profile() {
         </button>
       </div>
     </section>
+    
     <div className="max-w-6xl px-3 mt-6 mx-auto">
-    {!loading && listings.length > 0 && (
-      <>
-        <h2 className="text-2xl text-center font-semibold">{t('My Listings')}</h2>
-        <ul>
-          {listings.map((listing)=>(
-            <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
-          ))}
-        </ul>
-      </>
-    )}
-    </div>
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold mb-6">
+            {t("My Listings")}
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
